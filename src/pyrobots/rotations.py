858388bc8632db9a5396a46f3f5b93d1974f4db1
dario-loi <loi.1940849@studenti.uniroma1.it>
2023-11-11 16:59:41 +0100

    :bug: Fixed inverse rot for single matrix

--- src/pyrobots/rotations.py
+++ src/pyrobots/rotations.py
@@ -80,9 +80,15 @@ def direct_rot_mat(theta: float, axis: np.ndarray) -> np.ndarray:
 
 def inverse_rot_mat(
     R: np.ndarray,
-) -> Tuple[Union[float, Tuple[float, float]], Optional[np.ndarray]]:
+) -> Tuple[Tuple[float, float], Optional[np.ndarray]]:
     """
     This function returns the angle and axis of rotation for a given rotation matrix.
+
+    If the angle is 0, the axis is undefined and None is returned.
+
+    If the angle is pi or -pi, there are two possible solutions for the axis, and both are returned.
+
+    Otherwise, there are two possible solutions for the axis, and both are returned (order matters)
 
     Parameters
     ----------
@@ -91,8 +97,9 @@ def inverse_rot_mat(
 
     Returns
     -------
-    Tuple[Union[float, Tuple[float, float]], Optional[Vec3]]
-        Rotation angle in radians and rotation axis, the first can have either one or two values, the latter is None (undefined) if the angle is 0.
+    Tuple[Tuple[float, float], Optional[np.ndarray]]
+        Tuple containing the angle(s) and axis of rotation in radians.
+        If the axis is undefined, returns None.
     """
 
     assert R is not None, "R cannot be None"
@@ -113,12 +120,20 @@ def inverse_rot_mat(
             + (R[1, 0] - R[0, 1]) ** 2
         )
     )
-    theta = np.arctan2(2 * s, np.trace(R) - 1)
+    theta1, theta2 = np.arctan2(2 * s, np.trace(R) - 1), np.arctan2(
+        -2 * s, np.trace(R) - 1
+    )
 
-    if np.isclose(theta, 0):  # undefined axis
-        return 0, None
-    elif np.isclose(np.abs(theta), np.pi):
-        r = np.array(
+    assert np.isclose(theta1, theta2) or np.isclose(
+        theta1, -theta2
+    ), "[BUG] Theta1 and theta2 should be equal or opposite"
+
+    if np.isclose(theta1, 0) and np.isclose(theta2, 0):
+        return (0, 0), None
+    elif (np.isclose(theta1, np.pi) and np.isclose(theta2, -np.pi)) or (
+        np.isclose(theta1, -np.pi) and np.isclose(theta2, np.pi)
+    ):
+        r1 = np.array(
             [
                 np.sqrt((R[0, 0] + 1) / 2),
                 np.sqrt((R[1, 1] + 1) / 2),
@@ -126,16 +141,36 @@ def inverse_rot_mat(
             ]
         )
 
-        return (-theta, theta), r
-    else:  # theta != 0 and theta != pi
-        r = np.array(
+        r2 = np.array(
             [
-                (R[2, 1] - R[1, 2]) / (2 * s),
-                (R[0, 2] - R[2, 0]) / (2 * s),
-                (R[1, 0] - R[0, 1]) / (2 * s),
+                -np.sqrt((R[0, 0] + 1) / 2),
+                -np.sqrt((R[1, 1] + 1) / 2),
+                -np.sqrt((R[2, 2] + 1) / 2),
             ]
         )
-        return theta, r
+
+        return (theta1, theta2), (r1, r2)
+    else:  # theta != 0 and theta != pi
+        sin1 = np.sin(theta1)
+        sin2 = np.sin(theta2)
+
+        r1 = np.array(
+            [
+                (R[2, 1] - R[1, 2]) / (2 * sin1),
+                (R[0, 2] - R[2, 0]) / (2 * sin1),
+                (R[1, 0] - R[0, 1]) / (2 * sin1),
+            ]
+        )
+
+        r2 = np.array(
+            [
+                (R[2, 1] - R[1, 2]) / (2 * sin2),
+                (R[0, 2] - R[2, 0]) / (2 * sin2),
+                (R[1, 0] - R[0, 1]) / (2 * sin2),
+            ]
+        )
+
+        return (theta1, theta2), (r1, r2)
 
 
 def direct_rpy_separate(
